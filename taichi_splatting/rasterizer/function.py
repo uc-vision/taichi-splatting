Fix some instability with covariance axes computation

taichi_splatting/rasterizer/function.py
@@ -1,3 +1,7 @@
+from functools import cache
+
+from taichi_splatting.autograd import restore_grad
+
 from .forward import Config, forward_kernel
 from .backward import backward_kernel
 
@@ -7,55 +11,56 @@ from typing import Tuple
 import torch
 from beartype import beartype
 
+@cache
+def render_function(config:Config, num_features:int):
+  forward = forward_kernel(config, num_features)
+  backward = backward_kernel(config, num_features)
 
-class _module_function(torch.autograd.Function):
-  @staticmethod
-  def forward(ctx, gaussians: torch.Tensor, features: torch.Tensor,
-              overlap_to_point: torch.Tensor, tile_overlap_ranges: torch.Tensor,
-              image_size: Tuple[Integral, Integral], config: Config
-              ) -> torch.Tensor:
+  class _module_function(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, gaussians: torch.Tensor, features: torch.Tensor,
+                overlap_to_point: torch.Tensor, tile_overlap_ranges: torch.Tensor,
+                image_size: Tuple[Integral, Integral]
+                ) -> torch.Tensor:
+        
+      shape = (image_size[1], image_size[0])
+      image_feature = torch.zeros((*shape, features.shape[1]),
+                                  dtype=torch.float32, device=features.device)
+      image_alpha = torch.zeros(shape, dtype=torch.float32, device=features.device)
+      image_last_valid = torch.zeros(shape, dtype=torch.int32, device=features.device)
+
+      forward(gaussians, features, 
+        tile_overlap_ranges, overlap_to_point,
+        image_feature, image_alpha, image_last_valid)
+
+      # Non differentiable parameters
+      ctx.overlap_to_point = overlap_to_point
+      ctx.tile_overlap_ranges = tile_overlap_ranges
+      ctx.image_last_valid = image_last_valid
+      ctx.image_alpha = image_alpha
+
+      ctx.save_for_backward(gaussians, features, 
+                            image_feature)
       
-    shape = (image_size[1], image_size[0])
-    image_feature = torch.zeros((*shape, features.shape[1]),
-                                dtype=torch.float32, device=features.device)
-    image_alpha = torch.zeros(shape, dtype=torch.float32, device=features.device)
-    image_last_valid = torch.zeros(shape, dtype=torch.int32, device=features.device)
+      return image_feature
 
-    k = forward_kernel(config, features.shape[1])
-    k(gaussians, features, 
-      tile_overlap_ranges, overlap_to_point,
-      image_feature, image_alpha, image_last_valid)
+    @staticmethod
+    def backward(ctx, grad_image_feature:torch.Tensor):
+        gaussians, features, image_feature = ctx.saved_tensors
 
-    # Non differentiable parameters
-    ctx.overlap_to_point = overlap_to_point
-    ctx.tile_overlap_ranges = tile_overlap_ranges
-    ctx.image_last_valid = image_last_valid
-    ctx.image_alpha = image_alpha
-    ctx.config = config
+        grad_gaussians = torch.zeros_like(gaussians)
+        grad_features = torch.zeros_like(features)
 
-    ctx.save_for_backward(gaussians, features, 
-                          image_feature)
-    
-    return image_feature
+        with restore_grad(grad_gaussians, grad_features):
 
-  @staticmethod
-  def backward(ctx, grad_image_feature:torch.Tensor):
-      gaussians, features, image_feature = ctx.saved_tensors
+          backward(gaussians, features, 
+            ctx.tile_overlap_ranges, ctx.overlap_to_point,
+            image_feature, ctx.image_alpha, ctx.image_last_valid,
+            grad_image_feature,
+            grad_gaussians, grad_features)
 
-      grad_gaussians = torch.zeros_like(gaussians)
-      grad_features = torch.zeros_like(features)
-
-      k = backward_kernel(ctx.config, features.shape[1])
-
-      k(gaussians, features, 
-        ctx.tile_overlap_ranges, ctx.overlap_to_point,
-        image_feature, ctx.image_alpha, ctx.image_last_valid,
-        grad_image_feature,
-        grad_gaussians, grad_features)
-
-
-      return grad_gaussians, grad_features, None, None, None, None
-
+          return grad_gaussians, grad_features, None, None, None, None
+  return _module_function
 
 @beartype
 def rasterize(gaussians: torch.Tensor, features: torch.Tensor,
@@ -78,8 +83,8 @@ def rasterize(gaussians: torch.Tensor, features: torch.Tensor,
     Returns:
       image: (H, W, F) torch tensor, where H, W are the image height and width, F is the number of features
   """
-
+  _module_function = render_function(config, features.shape[1])
 
   return _module_function.apply(gaussians, features, 
           overlap_to_point, tile_overlap_ranges, 
-          image_size, config)
+          image_size)

taichi_splatting/renderer2d.py
@@ -9,15 +9,19 @@ from taichi_splatting.tile_mapper import map_to_tiles
 from taichi_splatting.rasterizer import rasterize, Config
 
 
+
 def project_gaussians2d(points: Gaussians2D) -> torch.Tensor:
     scale = torch.exp(points.log_scaling)
+
     alpha = torch.sigmoid(points.alpha_logit)
 
     v1 = points.rotation / torch.norm(points.rotation, dim=1, keepdim=True)
     v2 = torch.stack([-v1[..., 1], v1[..., 0]], dim=-1)
 
     basis = torch.stack([v1, v2], dim=2) * scale.unsqueeze(-1)
-    inv_cov = torch.inverse(torch.bmm(basis.transpose(1, 2), basis))
+    cov = torch.bmm(basis.transpose(1, 2), basis)
+    
+    inv_cov = torch.inverse(cov)
 
     conic = torch.stack([inv_cov[..., 0, 0], inv_cov[..., 0, 1], inv_cov[..., 1, 1]], dim=-1)
     return torch.cat([points.position, conic, alpha.unsqueeze(1)], dim=-1)  
@@ -36,28 +40,29 @@ def check_finite(tensor_dict):
     if n > 0:
       raise ValueError(f'Found {n} non-finite values in {k}')
 
+    if v.grad is not None:
+      n = (~torch.isfinite(v.grad)).sum()
+      if n > 0:
+        raise ValueError(f'Found {n} non-finite gradients in {k}')
 
 def render_gaussians(
       gaussians: Gaussians2D,
       image_size: Tuple[Integral, Integral],
-      tile_size: int = 16,
-      debug=False
+      tile_size: int = 16
     ):
   
-  if debug:
-    check_finite(gaussians)
 
   gaussians2d = project_gaussians2d(gaussians)
-  padded = pad_to_tile(image_size, tile_size)
+  padded_size = pad_to_tile(image_size, tile_size)
 
   overlap_to_point, ranges = map_to_tiles(gaussians2d, gaussians.depth, 
-    image_size=padded, tile_size=tile_size)
+    image_size=padded_size, tile_size=tile_size)
   
   raster_config = Config(tile_size=tile_size)
 
   image = rasterize(gaussians=gaussians2d, features=gaussians.feature, 
     tile_overlap_ranges=ranges, overlap_to_point=overlap_to_point,
-    image_size=padded, config=raster_config)
+    image_size=padded_size, config=raster_config)
   
   w, h = image_size
   return image[:h, :w]

taichi_splatting/scripts/fit_image_gaussians.py
@@ -1,25 +1,29 @@
 import math
+import random
 import cv2
 import argparse
+import numpy as np
 
 import torch
 from torch.optim import Adam
 
-from taichi_splatting.renderer2d import render_gaussians, Gaussians2D
+from taichi_splatting.renderer2d import check_finite, render_gaussians, Gaussians2D
 import taichi as ti
 
 def inverse_sigmoid(x:torch.Tensor):
   return torch.log(x / (1 - x))
 
 
-def random_2d_gaussians(n, image_size):
+def random_2d_gaussians(n, image_size, seed=0):
   w, h = image_size
 
-  scale = w / math.sqrt(n) 
+  scale =  0.25 * w / math.sqrt(n) 
 
   position = torch.rand(n, 2) * torch.tensor([w, h], dtype=torch.float32).unsqueeze(0)
   depth = torch.rand(n)  
-  scaling = (torch.rand(n, 2) + 0.5)  * scale
+  
+  # scaling = torch.full_like(position, scale)
+  scaling = (torch.rand(n, 2) + 0.5) * scale 
 
   rotation = torch.randn(n, 2) 
   rotation = rotation / torch.norm(rotation, dim=1, keepdim=True)
@@ -40,18 +44,21 @@ def random_2d_gaussians(n, image_size):
 def parse_args():
   parser = argparse.ArgumentParser()
   parser.add_argument('image_file', type=str)
+  parser.add_argument('--seed', type=int, default=0)
+  parser.add_argument('--tile_size', type=int, default=16)
+  
 
   return parser.parse_args()
 
 
-def optimizer(gaussians: Gaussians2D):
+def optimizer(gaussians: Gaussians2D, base_lr=1.0):
 
   learning_rates = dict(
-    position=0.00016,
-    log_scaling=0.005,
-    rotation=0.001,
-    alpha_logit=0.05,
-    feature=0.0025
+    position=0.01,
+    log_scaling=0.01,
+    rotation=0.01,
+    alpha_logit=0.2,
+    feature=0.01
   )
 
   params = {k: torch.nn.Parameter(x, requires_grad=True) 
@@ -60,22 +67,24 @@ def optimizer(gaussians: Gaussians2D):
 
 
   param_groups = [
-    dict(params=[params[name]], lr=param, name=name)
-      for name, param in learning_rates.items()
+    dict(params=[params[name]], lr=lr * base_lr, name=name)
+      for name, lr in learning_rates.items()
   ]
 
   return Adam(param_groups), Gaussians2D(**params, batch_size=gaussians.batch_size)
 
 
 def display_image(image):
-    image = (image * 255).to(torch.uint8)
+    image = (image.detach().clamp(0, 1) * 255).to(torch.uint8)
     image = image.cpu().numpy()
+
     cv2.imshow('rendered', image)
     cv2.waitKey(1)
     
 
+
 def main():
-  ti.init(arch=ti.cuda, log_level=ti.DEBUG)
+  ti.init(arch=ti.cuda, log_level=ti.INFO)
   device = torch.device('cuda:0')
 
   args = parse_args()
@@ -83,21 +92,28 @@ def main():
   h, w = ref_image.shape[:2]
 
   print(f'Image size: {w}x{h}')
-  cv2.namedWindow('rendered', cv2.WINDOW_GUI_EXPANDED)
+  cv2.namedWindow('rendered', cv2.WINDOW_FULLSCREEN)
 
+  torch.manual_seed(args.seed)
 
-  gaussians = random_2d_gaussians(100, (w, h)).to(torch.device('cuda:0'))
-  opt, params = optimizer(gaussians)
+  gaussians = random_2d_gaussians(20000, (w, h)).to(torch.device('cuda:0'))
+  opt, params = optimizer(gaussians, base_lr=1.0)
 
   ref_image = torch.from_numpy(ref_image).to(dtype=torch.float32, device=device) / 255
 
   while True:
     opt.zero_grad()
 
-    image = render_gaussians(params, (w, h), tile_size=16)
-    loss = torch.nn.functional.l1_loss(image, ref_image)
+    image = render_gaussians(params, (w, h), tile_size=args.tile_size)
+    loss = torch.nn.functional.l1_loss(image, ref_image) 
 
+    # s = params.log_scaling.exp()
+    # loss += (s.max(1).values / s.min(1).values).mean() * 0.01
+    
     loss.backward()
+
+
+    check_finite(params)
     opt.step()
 
     display_image(image)

taichi_splatting/spherical_harmonics.py
@@ -2,90 +2,13 @@ from functools import cache
 import math
 from beartype import beartype
 import taichi as ti
-from taichi.math import vec3
 import torch
 
 from taichi_splatting.autograd import restore_grad
+from taichi_splatting.ti.conversions import torch_taichi
 
 # Derived from torch-spherical-harmonics
 # https://github.com/cheind/torch-spherical-harmonics
-
-
-vec16f = ti.types.vector(16, ti.float32)
-vec9f = ti.types.vector(9,   ti.float32)
-vec4f = ti.types.vector(4,   ti.float32)
-vec1f = ti.types.vector(1,   ti.float32)
-
-@ti.func
-def rsh_cart_0(xyz: vec3) -> vec1f:
-    return vec1f(
-        0.282094791773878
-    )
-
-
-@ti.func
-def rsh_cart_1(xyz: vec3) -> vec4f:
-    x, y, z = xyz
-    return vec4f(
-        0.282094791773878,
-        -0.48860251190292 * y,
-        0.48860251190292 * z,
-        -0.48860251190292 * x,
-    )
-
-@ti.func
-def rsh_cart_2(xyz: vec3) -> vec9f:
-    x, y, z = xyz
-
-    x2 = x**2
-    y2 = y**2
-    z2 = z**2
-    xy = x * y
-    xz = x * z
-    yz = y * z
-
-    return vec9f(
-        0.282094791773878,
-        -0.48860251190292 * y,
-        0.48860251190292 * z,
-        -0.48860251190292 * x,
-        1.09254843059208 * xy,
-        -1.09254843059208 * yz,
-        0.94617469575756 * z2 - 0.31539156525252,
-        -1.09254843059208 * xz,
-        0.54627421529604 * x2 - 0.54627421529604 * y2,
-    )
-
-@ti.func
-def rsh_cart_3(xyz: vec3) -> vec16f:
-    x, y, z = xyz.x, xyz.y, xyz.z
-
-    x2 = x**2
-    y2 = y**2
-    z2 = z**2
-    xy = x * y
-    xz = x * z
-    yz = y * z
-
-    return vec16f(
-        0.282094791773878,
-        -0.48860251190292 * y,
-        0.48860251190292 * z,
-        -0.48860251190292 * x,
-        1.09254843059208 * xy,
-        -1.09254843059208 * yz,
-        0.94617469575756 * z2 - 0.31539156525252,
-        -1.09254843059208 * xz,
-        0.54627421529604 * x2 - 0.54627421529604 * y2,
-        -0.590043589926644 * y * (3.0 * x2 - y2),
-        2.89061144264055 * xy * z,
-        0.304697199642977 * y * (1.5 - 7.5 * z2),
-        1.24392110863372 * z * (1.5 * z2 - 0.5) - 0.497568443453487 * z,
-        0.304697199642977 * x * (1.5 - 7.5 * z2),
-        1.44530572132028 * z * (x2 - y2),
-        -0.590043589926644 * x * (x2 - 3.0 * y2),
-    )        
-
 
 def check_sh_degree(sh_features):
   n_sh = sh_features.shape[2]
@@ -94,20 +17,106 @@ def check_sh_degree(sh_features):
   assert n * n == n_sh, f"SH feature count must be square, got {n_sh} ({sh_features.shape})"
   return (n - 1)
 
-
 @cache
-def evaluate_sh_kernel(degree:int, dimension:int=3):
+def sh_function(degree:int=3, dimension:int=3, dtype=torch.float32):
+  ti_dtype = torch_taichi[dtype]
+
+
+  vec16 = ti.types.vector(16, ti_dtype)
+  vec9 = ti.types.vector(9,   ti_dtype)
+  vec4 = ti.types.vector(4,   ti_dtype)
+  vec3 = ti.types.vector(3,   ti_dtype)
+  vec1 = ti.types.vector(1,   ti_dtype)
+
+  @ti.func
+  def rsh_cart_0(_: vec3) -> vec1:
+      return vec1(
+          0.282094791773878
+      )
+
+
+  @ti.func
+  def rsh_cart_1(xyz: vec3) -> vec4:
+      x, y, z = xyz
+      return vec4(
+          0.282094791773878,
+          -0.48860251190292 * y,
+          0.48860251190292 * z,
+          -0.48860251190292 * x,
+      )
+
+  @ti.func
+  def rsh_cart_2(xyz: vec3) -> vec9:
+      x, y, z = xyz
+
+      x2 = x**2
+      y2 = y**2
+      z2 = z**2
+      xy = x * y
+      xz = x * z
+      yz = y * z
+
+      return vec9(
+          0.282094791773878,
+          -0.48860251190292 * y,
+          0.48860251190292 * z,
+          -0.48860251190292 * x,
+          1.09254843059208 * xy,
+          -1.09254843059208 * yz,
+          0.94617469575756 * z2 - 0.31539156525252,
+          -1.09254843059208 * xz,
+          0.54627421529604 * x2 - 0.54627421529604 * y2,
+      )
+
+  @ti.func
+  def rsh_cart_3(xyz: vec3) -> vec16:
+      x, y, z = xyz.x, xyz.y, xyz.z
+
+      x2 = x**2
+      y2 = y**2
+      z2 = z**2
+      xy = x * y
+      xz = x * z
+      yz = y * z
+
+      return vec16(
+          0.282094791773878,
+          -0.48860251190292 * y,
+          0.48860251190292 * z,
+          -0.48860251190292 * x,
+          1.09254843059208 * xy,
+          -1.09254843059208 * yz,
+          0.94617469575756 * z2 - 0.31539156525252,
+          -1.09254843059208 * xz,
+          0.54627421529604 * x2 - 0.54627421529604 * y2,
+          -0.590043589926644 * y * (3.0 * x2 - y2),
+          2.89061144264055 * xy * z,
+          0.304697199642977 * y * (1.5 - 7.5 * z2),
+          1.24392110863372 * z * (1.5 * z2 - 0.5) - 0.497568443453487 * z,
+          0.304697199642977 * x * (1.5 - 7.5 * z2),
+          1.44530572132028 * z * (x2 - y2),
+          -0.590043589926644 * x * (x2 - 3.0 * y2),
+      )        
+
+
+  def check_sh_degree(sh_features):
+    n_sh = sh_features.shape[2]
+    n = int(math.sqrt(n_sh))
+
+    assert n * n == n_sh, f"SH feature count must be square, got {n_sh} ({sh_features.shape})"
+    return (n - 1)
+
+
   assert degree >= 0 and degree <= 3
 
   rsh_cart_n = [rsh_cart_0, rsh_cart_1, rsh_cart_2, rsh_cart_3]
   rsh_cart = rsh_cart_n[degree]
 
-  param_mat = ti.types.matrix(n=dimension, m=(degree + 1)**2, dtype=ti.f32)
-  vec = ti.types.vector(n=dimension, dtype=ti.f32)
-
+  param_mat = ti.types.matrix(n=dimension, m=(degree + 1)**2, dtype=ti_dtype)
+  vec = ti.types.vector(n=dimension, dtype=ti_dtype)
 
   @ti.kernel    
-  def _evaluate_sh3_kernel(params:ti.types.ndarray(param_mat, ndim=1), 
+  def evaluate_sh_kernel(params:ti.types.ndarray(param_mat, ndim=1), 
                           dirs:ti.types.ndarray(vec3, ndim=1), 
                           out:ti.types.ndarray(vec, ndim=1)):
       
@@ -118,39 +127,39 @@ def evaluate_sh_kernel(degree:int, dimension:int=3):
           for d in ti.static(range(dimension)):
               out[i][d] = coeffs.dot(params_i[d, :])
 
-  return _evaluate_sh3_kernel
 
 
-class _module_function(torch.autograd.Function):
-  @staticmethod
-  def forward(ctx, params:torch.Tensor, dirs:torch.Tensor) -> torch.Tensor:
-      
-      degree = check_sh_degree(params)
-      ctx.kernel = evaluate_sh_kernel(degree, params.shape[1])
+  class _module_function(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, params:torch.Tensor, dirs:torch.Tensor) -> torch.Tensor:
+        
+        out = torch.zeros(dirs.shape[0], params.shape[1], dtype=dtype, device=params.device)
+        evaluate_sh_kernel(params, dirs, out)
 
-      out = torch.zeros(dirs.shape[0], params.shape[1], dtype=torch.float32, device=params.device)
-      ctx.kernel(params, dirs, out)
+        ctx.save_for_backward(params, dirs, out)
+        return out
 
-      ctx.save_for_backward(params, dirs, out)
-      return out
+    @staticmethod
+    def backward(ctx, doutput):
+        params, dirs, out = ctx.saved_tensors
 
-  @staticmethod
-  def backward(ctx, doutput):
-      params, dirs, out = ctx.saved_tensors
+        with restore_grad(params, dirs, out):
+          out.grad = doutput.contiguous()
+          evaluate_sh_kernel.grad(params, dirs, out)
 
-      with restore_grad(params, dirs, out):
-        out.grad = doutput.contiguous()
-        ctx.kernel.grad(params, dirs, out)
-
-        return params.grad, dirs.grad
-
+          return params.grad, dirs.grad
+        
+  return _module_function
 
 
 @beartype
 def evaluate_sh(params:torch.Tensor,  # N, K (degree + 1)^2,  (usually K=3, for RGB)
                 dirs:torch.Tensor     # N, 3
                 ) -> torch.Tensor:    # N, K
-    
+    degree = check_sh_degree(params)
+
+    _module_function = sh_function(degree=degree, 
+                                   dimension=params.shape[1], dtype=params.dtype)
     return _module_function.apply(params.contiguous(), dirs.contiguous())
 
 

taichi_splatting/tests/test_spherical_harmonics.py
@@ -12,14 +12,14 @@ warnings.filterwarnings('ignore')
 
 ti.init(debug=True)
 
-def random_inputs(seed, max_dim=3, max_deg=3, max_n=5, device='cpu'):
+def random_inputs(seed, max_dim=3, max_deg=3, max_n=5, device='cpu', dtype=torch.float32):
     torch.random.manual_seed(seed)
     dimension = torch.randint(1, max_dim, (1,)).item()
     degree = torch.randint(1, max_deg + 1, (1, )).item()
     n = torch.randint(1, max_n, (1,) ).item()
 
-    params = torch.rand(n, dimension, (degree + 1)**2, device=device, dtype=torch.float32)
-    dirs = torch.randn(n, 3, device=device, dtype=torch.float32)
+    params = torch.rand(n, dimension, (degree + 1)**2, device=device, dtype=dtype)
+    dirs = torch.randn(n, 3, device=device, dtype=dtype)
     dirs = torch.nn.functional.normalize(dirs, dim=1)
 
     return (params, dirs)
@@ -41,13 +41,13 @@ def test_sh(iters = 100, device='cpu'):
 
 def gradcheck(func, args, **kwargs):
   args = [x.requires_grad_(True) for x in args]
-  torch.autograd.gradcheck(func, args, **kwargs, eps=1e-2, atol=1e-3, rtol=1e-2)
+  torch.autograd.gradcheck(func, args, **kwargs)
 
 def test_sh_gradtest(iters = 100, device='cpu'):
 
-  seeds = torch.randint(0, 10000, (iters, ), device=device)
+  seeds = torch.randint(40, 10000, (iters, ), device=device)
   for seed in tqdm(seeds):
-      inputs = random_inputs(seed, device=device)
+      inputs = random_inputs(seed, device=device, dtype=torch.float64)
       gradcheck(taichi_sh.evaluate_sh, inputs)
 
 if __name__ == '__main__':