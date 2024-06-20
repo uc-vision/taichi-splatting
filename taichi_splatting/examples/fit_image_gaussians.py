5f07577a181a04b1a812a6588c7404a8980c9cd2
Oliver Batchelor <oliver.batchelor@canterbury.ac.nz>
2024-06-20 22:19:34 +1200

    .

--- taichi_splatting/examples/fit_image_gaussians.py
+++ taichi_splatting/examples/fit_image_gaussians.py
@@ -33,7 +33,7 @@ def parse_args():
   parser.add_argument('--n', type=int, default=1000)
   parser.add_argument('--target', type=int, default=None)
   parser.add_argument('--max_epoch', type=int, default=100)
-  parser.add_argument('--split_rate', type=float, default=0.1, help='Rate of pruning proportional to number of points')
+  parser.add_argument('--split_rate', type=float, default=0.5, help='Rate of pruning proportional to number of points')
   parser.add_argument('--opacity_reg', type=float, default=0.0001)
   parser.add_argument('--scale_reg', type=float, default=0.0001)
 
@@ -231,7 +231,7 @@ def main():
       cpsnr = psnr(ref_image, image)
       print(f'{epoch + 1}: {epoch_size / epoch_time:.1f} iters/sec CPSNR {cpsnr:.2f}')
 
-      if cmd_args.target:
+      if cmd_args.target and epoch < cmd_args.max_epoch - 1:
         gaussians = Gaussians2D(**params.tensors, batch_size=params.batch_size)
 
         t = (epoch + 1) / (cmd_args.max_epoch - 1)
@@ -240,7 +240,7 @@ def main():
         n = gaussians.batch_size[0]
 
         split_mask, prune_mask = split_prune(n = n, target = math.ceil(cmd_args.n * (1 - t_points) + t_points * cmd_args.target),
-                    n_prune=int(cmd_args.split_rate * n * (1 - t)**2),
+                    n_prune=int(cmd_args.split_rate * n * (1 - t)**3),
                     densify_score=densify_score, prune_cost=prune_cost)
 
     
