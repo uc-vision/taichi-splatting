1ac9a6759a1d6b430e7dcf8af7574938dfd6e21b
Oliver Batchelor <oliver.batchelor@canterbury.ac.nz>
2024-11-04 22:30:44 +1300

    .

--- taichi_splatting/examples/test_visibility.py
+++ taichi_splatting/examples/test_visibility.py
@@ -1,11 +1,12 @@
 
 
 from functools import partial
+from taichi_splatting.data_types import RasterConfig
 from taichi_splatting.misc.renderer2d import render_gaussians, split_gaussians2d, uniform_split_gaussians2d
 from taichi_splatting.tests.random_data import random_2d_gaussians
 import torch
 import taichi as ti
-
+import torch.nn.functional as F
 import cv2
 
 def display_image(name, image):
@@ -21,15 +22,19 @@ def main():
 
     torch.cuda.manual_seed(0)
     while True: 
-      gaussians = random_2d_gaussians(5, (640, 480), scale_factor=0.2, alpha_range=(1.0, 1.0)).cuda()
+      gaussians = random_2d_gaussians(1, (640, 480), scale_factor=0.2, alpha_range=(1.0, 1.0)).cuda()
 
 
-      rendering = render_gaussians(gaussians, (640, 480))
+
+      rendering = render_gaussians(gaussians, (640, 480), RasterConfig(compute_visibility=True, compute_point_heuristics=True))
       display_image('image', rendering.image)
-      splits = uniform_split_gaussians2d(gaussians, 2, random_axis=True)
 
-      rendering = render_gaussians(splits, (640, 480))
-      display_image('image', rendering.image)
+      err = F.mse_loss(rendering.image, rendering.image)
+      err.backward()
+
+      print(rendering.point_heuristics[:, 0])
+      print(rendering.visibility)
+
 
 if __name__ == "__main__":
     try:
