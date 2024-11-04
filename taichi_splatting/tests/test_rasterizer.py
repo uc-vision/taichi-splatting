Fix visibility calculation, add test (#20)
* Fix up tests, add visibility test

---------

Co-authored-by: Oliver Batchelor <oliver.batchelor@canterbury.ac.nz>

taichi_splatting/tests/test_rasterizer.py
@@ -59,7 +59,7 @@ def make_inputs(config, seed, device=torch.device('cuda:0')):
             colors.requires_grad_(True)), rasterize
 
 
-def test_rasterizer_gradcheck(name, config, show = False, iters = 100, device=torch.device('cuda:0')):
+def run_rasterizer_gradcheck(name, config, show = False, iters = 100, device=torch.device('cuda:0')):
   torch.random.manual_seed(0)
   seeds = torch.randint(0, 1000, (iters, ), device=device)
 
@@ -78,21 +78,26 @@ def test_rasterizer_gradcheck(name, config, show = False, iters = 100, device=to
       pbar.update(1)
       pbar.set_postfix(seed=seed.item())
 
+def test_antialias(show=False):
+  config = RasterConfig(tile_size=8, pixel_stride=(1, 1), antialias=True, use_alpha_blending=True)
+  run_rasterizer_gradcheck("antialias", config, show)
 
+def test_no_antialias(show=False):
+  config = RasterConfig(tile_size=8, pixel_stride=(1, 1), antialias=False, use_alpha_blending=True)
+  run_rasterizer_gradcheck("no_antialias", config, show)
+
+def test_no_blending(show=False):
+  config = RasterConfig(tile_size=8, pixel_stride=(1, 1), antialias=False, use_alpha_blending=False)
+  run_rasterizer_gradcheck("no_blending", config, show)
 
 def main(show=False, debug=False):
   torch.set_printoptions(precision=10, sci_mode=False)
-
   ti.init(arch=ti.cuda, default_fp=ti.f64, debug=debug)
 
-  config = RasterConfig(tile_size=8, pixel_stride=(1, 1), antialias=True, use_alpha_blending=True)
-  test_rasterizer_gradcheck("antialias", config, show)
+  test_antialias(show)
+  test_no_antialias(show)
+  test_no_blending(show)
 
-  config = RasterConfig(tile_size=8, pixel_stride=(1, 1), antialias=False, use_alpha_blending=True)
-  test_rasterizer_gradcheck("no_antialias", config, show)
-
-  config = RasterConfig(tile_size=8, pixel_stride=(1, 1), antialias=False, use_alpha_blending=False)
-  test_rasterizer_gradcheck("no_blending", config, show)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()

taichi_splatting/torch_lib/projection.py
@@ -19,7 +19,9 @@ def radii_from_cov(uv_cov:torch.Tensor):
 def eig(cov: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
   """ Compute the eigenvalues and eigenvectors of a 2x2 covariance matrix
   """
-  x, y, z = cov[..., 0], cov[..., 1], cov[..., 2]
+  assert cov.shape[-2:] == (2, 2), f"Expected ...x2x2 covariance matrix, got {cov.shape}"
+
+  x, y, z = cov[..., 0,0], cov[..., 0,1], cov[..., 1,1]
   tr = x + z
   det = x * z - y * y
 
@@ -29,7 +31,7 @@ def eig(cov: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
   lam1 = (tr + sqrt_gap) * 0.5
   lam2 = (tr - sqrt_gap) * 0.5
 
-  v1 = F.normalize(torch.stack([cov[..., 0] - lam2, cov[..., 1]], -1), dim=-1)
+  v1 = F.normalize(torch.stack([x - lam2, y], -1), dim=-1)
   v2 = torch.stack([-v1[..., 1], v1[..., 0]], -1)
 
   return torch.stack([lam1, lam2], -1).sqrt(), v1, v2
@@ -171,7 +173,7 @@ def apply(position, log_scaling, rotation, alpha_logit,
   
   sigma, v1, v2 = eig(cov)
   scale = sigma * gaussian_scale
-  lower, upper = ellipse_bounds(mean, v1 * scale[:, 0], v2 * scale[:, 1])
+  lower, upper = ellipse_bounds(mean, v1 * scale[:, 0:1], v2 * scale[:, 1:2])
 
     
   in_view = ((z > depth_range[0]) & (z < depth_range[1]) 