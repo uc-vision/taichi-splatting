29845b919c97cf458d3963255ae88297f802f78c
Oliver Batchelor <saulzar@gmail.com>
2024-11-06 01:58:44 +1300

    .

--- taichi_splatting/benchmarks/bench_rasterizer.py
+++ taichi_splatting/benchmarks/bench_rasterizer.py
@@ -8,6 +8,7 @@ from taichi_splatting.benchmarks.util import benchmarked
 
 from taichi_splatting.rasterizer.function import rasterize_with_tiles, RasterConfig
 from taichi_splatting.misc.renderer2d import project_gaussians2d
+from taichi_splatting.taichi_queue import TaichiQueue
 from taichi_splatting.tests.random_data import random_2d_gaussians
 from taichi_splatting.mapper.tile_mapper import map_to_tiles
 
