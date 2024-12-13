48abf7982c968f6d84a9a5db01d5e054d0875bef
Oliver Batchelor <saulzar@gmail.com>
2024-12-14 03:06:32 +1300

    Upgrade check_finite

--- taichi_splatting/torch_lib/util.py
+++ taichi_splatting/torch_lib/util.py
@@ -1,20 +1,43 @@
 import torch
-from typing import Mapping
+from typing import Mapping, Sequence
+from tensordict import TensorDict
+from tensordict.tensorclass import is_tensorclass
 
 def check_finite(t, name, warn=False):
-  
-  if isinstance(t, torch.Tensor):
-    n = (~torch.isfinite(t)).sum()
-    if n > 0:
-      if warn:
-        print(f'Found {n} non-finite values in {name}')
-        t[~torch.isfinite(t)] = 0
-      else:
-        raise ValueError(f'Found {n} non-finite values in {name}')
-    
-    if t.grad is not None:
-      check_finite(t.grad, f'{name}.grad', warn)
 
+  d = count_nonfinite(t, name, warn)
+  if len(d) > 0:
+    if warn:
+      print(f'Non-finite entries: {d}')
+    else:
+      raise ValueError(f'Non-finite entries: {d}')
+  
+  
+
+def count_nonfinite(t, name, warn=False) -> dict:
+  d = {}
+
+  if isinstance(t, torch.Tensor):
+    d[name] = (~torch.isfinite(t)).sum()
+    if t.grad is not None:
+      d[f'{name}.grad'] = count_nonfinite(t.grad, f'{name}.grad', warn)
+    return d
+  
+  if isinstance(t, Sequence) and not isinstance(t, str):
+    d = {}
+    for i, v in enumerate(t):
+      d.update(count_nonfinite(v, f'{name}[{i}]', warn))
+    return d
+  
   if isinstance(t, Mapping):
+    d = {}
     for k, v in t.items():
-      check_finite(v, f'{name}.{k}', warn)
+      d.update(count_nonfinite(v, f'{name}.{k}', warn))
+    return d
+
+  if is_tensorclass(t):
+    for k, v in t.items():
+      d.update(count_nonfinite(v, f'{name}.{k}', warn))
+    return d
+
+  return {}
